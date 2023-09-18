5d2ad16cef8b3ded3c842f58c6a2b24a17f5b10f
szabobalazs99 <gmtigu@hallgato.nje.hu>
2023-09-18 15:11:34 +0200

    Update builtin_wrappers.py

--- src/utils/builtin_wrappers.py
+++ src/utils/builtin_wrappers.py
@@ -1,8 +1,5 @@
 import collections
 
 
-def every_nth(input_list, step_size):
-    if step_size < 1:
-        print('Step size smaller than 1, input list returned')
-        return input_list
-    return input_list[::step_size]
+def contains_values(input_list, element):
+    return element in input_list
