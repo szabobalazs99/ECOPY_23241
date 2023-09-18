12c8ad2e41be60e66fe0278bab1043e312a7c820
szabobalazs99 <gmtigu@hallgato.nje.hu>
2023-09-18 14:28:20 +0200

    gyakorlas

--- src/utils/builtin_wrappers.py
+++ src/utils/builtin_wrappers.py
@@ -1,1 +1,8 @@
 import collections
+
+
+def every_nth(input_list, step_size):
+    if step_size < 1:
+        print('Step size smaller than 1, input list returned')
+        return input_list
+    return input_list[::step_size]
